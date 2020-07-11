358951fdae05e2c0452a02b6ab34a40e5133343e
Heike Winkelvoß <public@egladil.de>
2020-07-11 07:52:09 +0200

    TokenExchangeType added

--- projects/hewi-ng-lib/src/lib/jwt/jwt.model.ts
+++ projects/hewi-ng-lib/src/lib/jwt/jwt.model.ts
@@ -1,8 +1,11 @@
+export type TokenExchangeType = 'IMPLICITE_FLOW' | 'AUTHORIZATION_TOKEN_GRANT';
+
 export interface AuthResult {
 	expiresAt?: number;
 	state?: string;
 	nonce?: string;
 	idToken: string;
+	tokenExchangeType?: string;
 }
 
 
